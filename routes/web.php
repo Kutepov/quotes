3aebebd599fcba982a371e7714ee1be1ef04fc31
Kutepov <vadim.kutepov@gmail.com>
2022-02-23 18:49:03 +0500

    Bugfix

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', function () {
